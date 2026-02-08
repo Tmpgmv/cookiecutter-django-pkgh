a39280c75550f093f4d65e9e2fdbb90f473952e1
Your Name <you@example.com>
2026-02-08 23:31:29 +0300

    tmp

--- {{ cookiecutter.project_slug }}/general/context_processors.py
+++ {{ cookiecutter.project_slug }}/general/context_processors.py
@@ -1,7 +1,7 @@
 # PREP {
 def project_name(request):
-    return {"PROJECT_NAME": {{ cookiecutter.project_name_rus }}}
+    return {"PROJECT_NAME": "{{ cookiecutter.project_name_rus }}"}
 
 def project_description(request):
-    return {"PROJECT_DESCRIPTION": {{ cookiecutter.project_description }}}
+    return {"PROJECT_DESCRIPTION": "{{ cookiecutter.project_description }}"}
 # } PREP
