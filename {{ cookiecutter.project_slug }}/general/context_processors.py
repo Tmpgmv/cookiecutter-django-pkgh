cc4c648c4c689d429fac38ae733db6bbd1dbb353
Your Name <you@example.com>
2026-02-09 12:06:35 +0300

    tmp

--- {{ cookiecutter.project_slug }}/general/context_processors.py
+++ {{ cookiecutter.project_slug }}/general/context_processors.py
@@ -1,7 +1,12 @@
 # PREP {
+def sanitize(a_string):
+    result = a_string.replace('"', '\\"').replace("'", "\\'")
+    return result
+
+
 def project_name(request):
-    return {"PROJECT_NAME": "{{ cookiecutter.project_name_rus }}"}
+    return {"PROJECT_NAME": sanitize("{{ cookiecutter.project_name_rus }}")}
 
 def project_description(request):
-    return {"PROJECT_DESCRIPTION": "{{ cookiecutter.project_description }}"}
+    return {"PROJECT_DESCRIPTION": sanitize("{{ cookiecutter.project_description }}")}
 # } PREP
