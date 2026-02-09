1ec77826d542044e3a72da5a1e764bcf7e5d9d9a
Your Name <you@example.com>
2026-02-09 20:34:33 +0300

    tmp

--- {{ cookiecutter.project_slug }}/general/context_processors.py
+++ {{ cookiecutter.project_slug }}/general/context_processors.py
@@ -1,5 +1,5 @@
 def project_context(request):
     return {
-        "PROJECT_NAME": "{{ cookiecutter.project_name }}",
+        "PROJECT_NAME": "{{ cookiecutter.project_name_rus }}",
         "PROJECT_DESCRIPTION": "{{ cookiecutter.project_description }}",
     }
