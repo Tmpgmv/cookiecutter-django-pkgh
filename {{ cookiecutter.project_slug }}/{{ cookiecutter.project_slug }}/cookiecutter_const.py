265cf9361c593dfa6ba4841f5f9f359607b422d7
Your Name <you@example.com>
2026-02-12 22:14:07 +0300

    tmp

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/cookiecutter_const.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/cookiecutter_const.py
@@ -1,7 +1,7 @@
 # PREP {
-STUDENT_FULL_NAME_RUS = "{{ cookiecutter.student_full_name_rus|replace('\"', '\\\"')|replace(''', '\\'')|trim() }}"
-PROJECT_NAME_RUS = "{{ cookiecutter.project_name_rus|replace('\"', '\\\"')|replace(''', '\\'')|trim() }}"
-PROJECT_DESCRIPTION = "{{ cookiecutter.project_description|replace('\"', '\\\"')|replace(''', '\\'')|trim() }}"
+STUDENT_FULL_NAME_RUS = r"{{ cookiecutter.student_full_name_rus|replace('\"', '\\\"')|replace('\'', '\\\'')|trim() }}"
+PROJECT_NAME_RUS = r"{{ cookiecutter.project_name_rus|replace('\"', '\\\"')|replace('\'', '\\\'')|trim() }}"
+PROJECT_DESCRIPTION = r"{{ cookiecutter.project_description|replace('\"', '\\\"')|replace('\'', '\\\'')|trim() }}"
 MAIN_BACKGROUND_COLOR = "{{ cookiecutter.main_background_color|trim() }}"
 AUX_BACKGROUND_COLOR = "{{ cookiecutter.aux_background_color|trim() }}"
 ATTENTION_COLOR = "{{ cookiecutter.attention_color|trim() }}"
