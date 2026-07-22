ddcc6064e3b0aff2d80a64e5b760f902f5980f8c
Tmpgmv <75039513+Tmpgmv@users.noreply.github.com>
2026-07-22 22:31:43 +0300

    Update view_mixins.py

--- {{ cookiecutter.project_slug }}/general/view_mixins.py
+++ {{ cookiecutter.project_slug }}/general/view_mixins.py
@@ -12,3 +12,40 @@ class ConcurrentUpdateMixin:
                            "Вернитесь в список и откройте заново."
                            )
             return self.form_invalid(form)
+
+
+
+class GetVerboseNameMixin:
+    """
+    В шаблонах general (в частности, detail.html, primitive_list.html)
+    выводятся данные об объектах.
+
+    Для заголовка и т.п. хочется знать,
+    как же называется модель, которую мы отображаем.
+
+    Которая model:
+
+    class ArticleDetailView(DetailView):
+        model = Article
+
+    class ArticleListView(ListView):
+        model = Article
+
+
+    Для этого модель должна иметь соответствующие данные в классе Meta:
+
+    class Plane(models.Model):
+
+        ...
+
+        class Meta:
+            verbose_name = "Cамолет"
+            verbose_name_plural = "Cамолеты"
+    """
+
+
+    def get_verbose_name_plural(self):
+        return self.model._meta.verbose_name_plural
+
+    def get_verbose_name(self):
+        return self.model._meta.verbose_name
