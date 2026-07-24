3cbeadab70ea979aa74fcb1ff3646424bb26d39a
Tmpgmv <75039513+Tmpgmv@users.noreply.github.com>
2026-07-24 11:01:18 +0300

    Update search-sort-filter.js

--- {{ cookiecutter.project_slug }}/general/static/general/js/search-sort-filter.js
+++ {{ cookiecutter.project_slug }}/general/static/general/js/search-sort-filter.js
@@ -1,3 +1,7 @@
+// Автоматическая отправка формы фильтрации, сортировки и поиска без перезагрузки страницы.
+// См. комментарий в templates/general/embedded/search_sort_filter.html
+
+
 $('#id_sort, #id_filter').on('change', function() {
         $('#search-sort-filter').submit();
     });
