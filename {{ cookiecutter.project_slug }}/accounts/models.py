dcb38e90530ad724dfb1c461582b7f8d7e0efd34
Tmpgmv <75039513+Tmpgmv@users.noreply.github.com>
2026-07-25 14:52:29 +0300

    Update models.py

--- {{ cookiecutter.project_slug }}/accounts/models.py
+++ {{ cookiecutter.project_slug }}/accounts/models.py
@@ -14,22 +14,50 @@ class User(AbstractUser):
                                   verbose_name="Отчество")
 
     def get_full_name(self):
+        """
+        Получить полное ФИО.
+        :return: str
+        """
         return super().first_name + " " + self.patronymic + " " + super().last_name
 
     def is_admin(self):
+        """
+        Проверяет, является ли текущий пользователь администратором.
+
+        :return: bool
+        """        
         result = self.groups.filter(name='Администратор').exists() or self.is_superuser
         return result
 
     def is_manager(self):
+        """
+        Проверяет, является ли текущий пользователь менеджером.
+
+        :return: bool
+        """        
         return self.groups.filter(name='Менеджер').exists()
 
     def is_client(self):
+        """
+        Проверяет, является ли текущий пользователь авторизованным клиентом.
+
+        :return: bool
+        """        
         return self.groups.filter(name__icontains='авторизированный').exists()        
 
     def __str__(self):
         return self.get_full_name()
 
     def role(self):
+        """
+        Получить роль пользователя.
+        Пользователь может иметь множество ролей.
+        Но в интерфейсе надо отобразить одну - основную - 
+        роль пользователя. Поэтому метод 
+        возвращает одну роль.
+         
+        :return: Group или ""
+        """        
         return self.groups.first() or ""
 
     class Meta:
