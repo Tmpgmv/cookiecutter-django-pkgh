3abadc34b5794273be39e70058758fe3c932aeca
Tmpgmv <75039513+Tmpgmv@users.noreply.github.com>
2026-07-25 08:48:33 +0300

    Update settings.py

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     # PREP {
+    "django_model_info.apps.DjangoModelInfoConfig",
     "jquery",
     "django_bootstrap_icons",
 
